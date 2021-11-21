c797f8d7cdcf52602048475e4dc18a99d8fbf320
Lijun Liao <lijun.liao@gmail.com>
2021-11-21 21:08:57 +0100

    fixed CMD

--- disclose-jks-password/src/assembly/unfiltered/benchmark.bat
+++ disclose-jks-password/src/assembly/unfiltered/benchmark.bat
@@ -1,12 +1,13 @@
+@ECHO OFF
+
 SET DIR=%CD%
 
 SET MAIN_CLASS=org.xipki.jksfail.FindPasswordBenchmark
 
 if "x%JAVA_HOME%" == "x" (
   SET JAVA_EXEC=java
-  )
-else (
+) else (
   SET JAVA_EXEC="%JAVA_HOME%\bin\java"
-  )
+)
 
 %JAVA_EXEC% -cp %DIR%\lib;%DIR%\lib\* %MAIN_CLASS% %*
--- disclose-jks-password/src/assembly/unfiltered/run.bat
+++ disclose-jks-password/src/assembly/unfiltered/run.bat
@@ -1,3 +1,5 @@
+@ECHO OFF
+
 SET DIR=%CD%
 
 REM Example: run.bat -d examples\password-dict.txt -k examples\keystore-rsa.jks
@@ -6,9 +8,8 @@ SET MAIN_CLASS=org.xipki.jksfail.Main
 
 if "x%JAVA_HOME%" == "x" (
   SET JAVA_EXEC=java
-  )
-else (
+) else (
   SET JAVA_EXEC="%JAVA_HOME%\bin\java"
-  )
+)
 
 %JAVA_EXEC% -cp %DIR%\lib;%DIR%\lib\* %MAIN_CLASS% %*
